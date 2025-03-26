1c94be8f61550e058b89b029ec29810cd10bc9bb
m1toi <147705211+m1toi@users.noreply.github.com>
2025-03-27 00:04:13 +0200

    create login and register components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import Nora from '@primeng/themes/nora';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Nora
+      }
+    })]
 };
