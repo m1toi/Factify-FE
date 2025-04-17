a7315fcb8a983e1cba7aecf1c1ee7143c90f23d0
m1toi <147705211+m1toi@users.noreply.github.com>
2025-04-17 18:47:40 +0300

    implement create-post component

--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PostService } from '../../services/post.service';
+import { CategoryService } from '../../services/category.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
-  imports: [],
+  standalone: true,
   templateUrl: './create-post.component.html',
-  styleUrl: './create-post.component.scss'
+  styleUrls: ['./create-post.component.scss'],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    DropdownModule,
+    ButtonModule,
+    FormsModule
+  ]
 })
-export class CreatePostComponent {
+export class CreatePostComponent implements OnInit {
+  postForm!: FormGroup;
+  categories: any[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    private postService: PostService,
+    private categoryService: CategoryService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.postForm = this.fb.group({
+      question: ['', Validators.required],
+      answer: ['', Validators.required],
+      categoryId: [null, Validators.required],
+    });
+
+    this.categoryService.getAllCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  submitPost() {
+    if (this.postForm.valid) {
+      this.postService.createPost(this.postForm.value).subscribe({
+        next: () => this.router.navigate(['/home']),
+        error: err => console.error('Post creation failed:', err),
+      });
+    }
+  }
 }
