0102319977d35f218e4977d2fee0aca45d8fd94c
m1toi <147705211+m1toi@users.noreply.github.com>
2025-05-25 13:29:25 +0300

    update bubble to handle posts

--- src/app/pages/message-bubble/message-bubble.component.ts
+++ src/app/pages/message-bubble/message-bubble.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Message } from '../../services/message.service';
+import {PostCardComponent} from '../post-card/post-card.component';
 
 @Component({
   selector: 'app-message-bubble',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PostCardComponent],
   templateUrl: './message-bubble.component.html',
   styleUrls: ['./message-bubble.component.scss']
 })
 export class MessageBubbleComponent {
   @Input() message!: Message;
   @Input() isMine = false;
+
+  flipped = false;
 }
